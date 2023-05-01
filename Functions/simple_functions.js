bd46b8e7d58e73f7a72e6a5a4a9028c00b0e9436
Alejandro Regus <Aregus@appdev.yearup.org>
2023-05-01 10:28:06 -0400

    simple-functions-exercises-complete

---
+++ Functions/simple_functions.js
@@ -0,0 +1,17 @@
+function favoritethings() {
+console.log("My name is Alejandro Regus");
+console.log("Spider-Man 2");
+console.log("Gorillaz");
+}
+
+function whyImHere() {
+    console.log("I'm here to gain the experience i need to get a career in coding to become financially stable for the rest of my life.")
+}
+
+function favoriteplaces() {
+    console.log("A place i love to visit is my hometown of Lawrence, Massachussetts. I love meeting up with old families and friends.")
+}
+
+favoritethings();
+whyImHere();
+favoriteplaces();
